3c21871f74a268d93707934224aa2230d259a76f
Leticia Daniele Perotti <80782049+leticia-perotti@users.noreply.github.com>
2022-09-10 09:59:04 -0300

    Tornando um elemento como atributo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  //styleUrls: ['./app.component.scss']
+  styles: [`
+  h3{
+    color: #876330;
+  }`]
 })
 export class AppComponent {
-  title = 'minhas-receitas';
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { ServerComponent } from './server/server.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ServerComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
